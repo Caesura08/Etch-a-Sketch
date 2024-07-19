c1c1b34d6ca31638d11ff7e096c4214c1a3f0884
Caesura08 <jasmcknight233@gmail.com>
2024-07-19 09:21:23 -0700

    finished html/js for etch-a-sketch game

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
 const gridContainer = document.getElementById("gridContainer");
-for (let i = 0; i < 16; i++) {
-    for (let y = 0; y < 16; y++) {
-    const cell = document.createElement("div");
-    cell.classList.add("cell");
-    gridContainer.appendChild(cell);
-    }
+let rows = 16;
+let col = 16;
+
+function createGrid () {
+    rows = parseInt(document.getElementById("rows").value);
+    col = parseInt(document.getElementById("col").value);
+    gridContainer.innerHTML = '';
+    gridContainer.style.gridTemplateColumns = "repeat(${cols}, 20px"
+    for (let i = 0; i < rows * col; i++) {
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        cell.addEventListener("mouseover", mouseEvent => {
+            cell.classList.add("active");
+        });
+        cell.addEventListener("mouseout", mouseEvent => {
+            cell.classList.remove("active");
+        });
+        gridContainer.appendChild(cell);
+        cell.addEventListener("pointerdown", PointerEvent => {
+            cell.style.backgroundColor = "pink";
+        });
+        cell.addEventListener("dblclick", MouseEvent => {
+            cell.style.backgroundColor = "white";
+        })
+    }; 
 }
+
+function clearGrid () {
+    const cells = gridContainer.querySelectorAll(".cell");
+    cells.forEach(cell => {
+        cell.style.backgroundColor = "";
+    });
+}
+
+createGrid ();
+
 
 
 
